9f5974ab32b5af1dc7a90315f349bbe836a3e992
yuka hatakeyama <yuka@yuka-no-iMac.local>
2018-07-18 04:19:38 +0900

    修正

--- Picamera/cap_pi.py
+++ Picamera/cap_pi.py
@@ -17,7 +17,7 @@ from time import sleep
 # VideoCaptureのインスタンスを作成する。
 # 引数でカメラを選べれる。
 # 0は内蔵カメラ、1は入力カメラ
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 cap.set(4, 700)  # Width
 cap.set(4, 600)  # Heigh
 cap.set(5, 15)   # FPS
@@ -31,7 +31,7 @@ output_name = output_path + '/' + setnumber
 
 # ディレクトリ確認用(うまく行かなかった時用)
 import os
-print(os.path.exists(output_path))
+print((os.path.exists(output_path)))
 
 print('start')
 
