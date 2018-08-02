8307568dacc11def430b495c9e7ee0c0e0f7f57a
yuka hatakeyama <yuka@yuka-no-iMac.local>
2018-08-02 14:18:49 +0900

    動画取得方法修正

---
+++ Picamera/cap_pi2.py
@@ -0,0 +1,69 @@
+# Webカメラ 画像取得コード　New!8/2
+
+import cv2
+import glob
+import os
+import sys
+import time
+from os import getpid
+from os import kill
+from threading import Timer
+from time import sleep
+
+# VideoCaptureのインスタンスを作成する。
+# 引数でカメラを選べれる。
+# 0は内蔵カメラ、1は入力カメラ
+cap = cv2.VideoCapture(0)
+#cap.set(4, 700)  # Width
+#cap.set(4, 600)  # Heigh
+#cap.set(5, 20)   # FPS
+
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
+# ディレクトリパス指定
+sys.path.append('Desktop/iMac-Desktop/git/research_test/raspy/images')
+
+setnumber = "cap_img"
+output_path = "./images"
+output_name = output_path + '/' + setnumber
+
+# ディレクトリ確認用(うまく行かなかった時用)
+import os
+print(os.path.exists(output_path))
+
+print('start')
+
+i = 1;
+while True:
+    # VideoCaptureから1フレーム読み込む
+    ret, frame = cap.read()
+
+    if ret==True:
+        frame = cv2.flip(frame,180)
+        out.write(frame)
+
+        # 動画を取得
+        cv2.imshow('Frame',frame)
+
+        # sleep を使う方法
+        time.sleep(15)
+
+        # raspy/image ディレクトリに取得画像を自動保存
+        cv2.imwrite(output_name + str(i) + ".png", frame)
+        print(setnumber + str(i) + ".png")
+        i += 1
+
+        #k = cv2.waitKey(1)
+        #if k == 27:
+        #    break
+
+    else:
+        break
+
+print('end')
+
+# キャプチャをリリースして、ウィンドウをすべて閉じる
+cap.release()
+out.release()
+cv2.destroyAllWindows()
